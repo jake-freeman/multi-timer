71c471dd4c4a36c94b3fe4124697617c1ad9feac
jake-freeman <Jacob.Freeman@vorne.com>
2019-12-27 14:34:37 -0600

    Get the bundler working

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-const rootElement = document.getElementById('body');
+const rootElement = document.getElementById('root');
 
 // Render the UI
 ReactDOM.render(
